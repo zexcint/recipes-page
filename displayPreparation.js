34de0cd6443eb164c75df29b64f8b8166c2e3d89
zexcint <zexcint365@gmail.com>
2024-11-08 21:21:07 -0300

    v1.5 solved pdf download

--- displayPreparation.js
+++ displayPreparation.js
@@ -2,6 +2,7 @@ export { displayPreparation, resetPreparationValues, startClosingAnimation }
 import { createFragment, timeOut } from "./main.js"
 import { URL, getDetailsById, getFlags } from "./apiFunctions.js"
 import { DOM, CLASS } from "./global.js"
+const { jsPDF } = window.jspdf;
 
 const defaultBtn = () => DOM.TEMPLATE[4].cloneNode(true)
 const downloadBtn = () => DOM.TEMPLATE[6].cloneNode(true)
@@ -113,40 +114,64 @@ const startClosingAnimation = async () => {
 }
 
 
-async function saveToPdf() {
-  const newWindow = window.open()
-  const head = document.createElement("head")
-  const html = document.createElement("html")
-  const body = document.createElement("body")
-  const title = document.createElement("title")
-  const h1 = document.createElement("h1")
-  const ol = document.createElement("h1")
-  const article = document.createElement("article")
+// function saveToPdf() {
+//   const newWindow = window.open()
+//   const head = document.createElement("head")
+//   const html = document.createElement("html")
+//   const body = document.createElement("body")
+//   const title = document.createElement("title")
+//   const h1 = document.createElement("h1")
+//   const ol = document.createElement("ol")
+//   const article = document.createElement("article")
 
-  h1.textContent = `${DOM.RECIPE_PREPARATION_H2().dataset.id.toUpperCase()}:`
-  title.textContent = DOM.RECIPE_PREPARATION_H2().dataset.id.toUpperCase()
-  head.append(title)
+//   h1.textContent = `${DOM.RECIPE_PREPARATION_H2().dataset.id.toUpperCase()}:`
+//   title.textContent = DOM.RECIPE_PREPARATION_H2().dataset.id.toUpperCase()
+//   head.append(title)
 
-  for (let index = 0; index < DOM.RECIPE_PREPARATION_OL().childElementCount; index++) {
-    const li = document.createElement("li")
-    li.textContent = `${DOM.RECIPE_PREPARATION_OL().children[index].textContent}`
-    if (li.textContent !== "") {
-      ol.append(li)
-    }
+//   for (let index = 0; index < DOM.RECIPE_PREPARATION_OL().childElementCount; index++) {
+//     const li = document.createElement("li")
+//     li.textContent = `${DOM.RECIPE_PREPARATION_OL().children[index].textContent}`
+//     if (li.textContent !== "") {
+//       ol.append(li)
+//     }
+//   }
+
+//   const paragraph  = [...DOM.RECIPE_PREPARATION_SECTION().children]
+//   paragraph.forEach(p => {
+//     article.append(p)
+//   })
+
+//   body.append(h1, ol, article)
+//   html.append(head, body)
+//   newWindow.document.write(html.innerHTML)
+//   newWindow.document.close()
+//   newWindow.print()
+//   newWindow.close()
+// }
+
+function saveToPdf() {
+  const doc = new jsPDF();
+  const longitude = DOM.RECIPE_PREPARATION_OL().childElementCount
+  let name = `${DOM.RECIPE_PREPARATION_H2().dataset.id.toUpperCase()}:\n`
+  let ingredients = ""
+  let preparation = ""
+
+  for (let i = 0; i < longitude; i++) {
+    ingredients  += `${DOM.RECIPE_PREPARATION_OL().children[i].textContent}\n`
   }
 
   const paragraph  = [...DOM.RECIPE_PREPARATION_SECTION().children]
+
   paragraph.forEach(p => {
-    article.append(p)
+    preparation += `${p.textContent}\n`
   })
 
-  body.append(h1, ol, article)
-  html.append(head, body)
-  newWindow.document.write(html.innerHTML)
-  newWindow.document.close()
-  await timeOut(1000)
-  newWindow.print()
-  newWindow.close()
+  doc.text(name, 10, 10, { maxWidth: 200 });
+  doc.text(ingredients, 10, 30, { maxWidth: 200 });
+  doc.addPage()
+  doc.text(preparation, 10, 10, { maxWidth: 200 });
+
+  doc.save(name)
 }
 
 const formatText = () => {
@@ -170,9 +195,9 @@ const formatText = () => {
   for (let i = 0; i < textModified.length; i++) {
     const p = document.createElement("p")
     if ((i + 1) < 10) {
-      p.textContent = `0${i + 1}:\t${textModified[i].trim()}.`
+      p.textContent = `0${i + 1}: ${textModified[i].trim()}.`
     } else {
-      p.textContent = `${i + 1}:\t${textModified[i].trim()}.`
+      p.textContent = `${i + 1}: ${textModified[i].trim()}.`
     }
     fragment.append(p)
   }
